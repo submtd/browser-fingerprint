00d5c00d8cdca5ea0d5f54e5ece646290e5732df
Steve Harmeyer <harmeyersteve@gmail.com>
2019-03-28 12:17:21 -0700

    updates

--- src/Fingerprint.php
+++ src/Fingerprint.php
@@ -4,13 +4,32 @@ namespace Submtd\BrowserFingerprint;
 
 class Fingerprint
 {
-    public function __invoke()
+    public $getServerVariables = [
+        'HTTP_USER_AGENT',
+        'HTTP_ACCEPT',
+        'REMOTE_ADDR',
+    ];
+
+    public $fingerprint = [];
+
+    public function __invoke(array $serverVariables = null)
     {
-        return 'hello, world!';
+        return get_browser();
+        if ($serverVariables) {
+            $this->getServerVariables = $serverVariables;
+        }
+        foreach ($this->getServerVariables as $variable) {
+            if (isset($_SERVER[$variable])) {
+                $this->fingerprint[$variable] = $_SERVER[$variable];
+            }
+        }
+        return $_SERVER;
+        return $this->fingerprint;
     }
 
-    public static function get()
+    public static function get(array $serverVariables = null)
     {
-        return $this();
+        $fingerprint = new static($serverVariables);
+        return $fingerprint();
     }
 }
